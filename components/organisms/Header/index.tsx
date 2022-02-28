1551acd2cfd0549b0771149f3dabf680d81e63a8
SnowSuno <snowsuno.dev@gmail.com>
2022-02-28 21:06:27 +0900

    fix : add header margin

--- components/organisms/Header/index.tsx
+++ components/organisms/Header/index.tsx
@@ -5,14 +5,15 @@ function Header ()  {
     const [count, setCount] = React.useState(0);
 
 
-    return (
+    return (<>
         <header className={styles.container}>
             this is header
             <button onClick={() => setCount(count + 1)}>{count}</button>
 
 
         </header>
-    )
+        <div className={styles.placeholder}/>
+    </>)
 }
 
 export default Header;
